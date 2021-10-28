5bbef2cd6eb20df597b812f81e26fa15fbd004ff
Jaclyn Karasik <jkarasik@unc.edu>
2021-10-27 23:38:27 -0400

    Completed sample shiny viz of median rent prices for HW10.

---
+++ R_Shiny.R
@@ -0,0 +1,45 @@
+
+# Setup -------------------------------------------------------------------
+
+install.packages("shiny");
+rawdata <- read.csv("~/work/NYC_Housing_Data.csv")
+
+# RShiny Visualization ----------------------------------------------------
+
+#UI
+library(shiny)
+
+# Define UI for app that draws a histogram ----
+ui <- fluidPage(
+  titlePanel("NYC Housing Data"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput(inputId = "bins",
+                  label = "Number of bins:",
+                  min = 1,
+                  max = 20,
+                  value = 100)
+    ),
+    
+    mainPanel(
+      plotOutput(outputId = "distPlot")
+      
+    )
+  )
+)
+
+#Server
+server <- function(input, output) {
+  output$distPlot <- renderPlot({
+    x    <- rawdata$median_rent
+    bins <- seq(min(x), max(x), length.out = input$bins)
+    
+    hist(x, breaks = bins, col = "blue", border = "white",
+         xlab = "Median Rent Price",
+         ylab = "Number of boros",
+         main = "Histogram of Median Rent Prices in NYC Boros")
+    
+  })
+}
+# Start the Server
+shinyApp(ui=ui, server=server, options=list(port=8080, host="0.0.0.0"))
