f8ac97a5b9e9e64edd13942e9483cdc480ed8988
Jaclyn Karasik <jkarasik@unc.edu>
2021-10-29 12:03:44 -0400

    added csv target to end of file.

--- clean_data.R
+++ clean_data.R
@@ -1,15 +1,7 @@
 library(tidyverse);
+source("~/work/utils.R");
+
 rawdata <- read.csv("~/work/NYC_Housing_Data.csv");
-
-count(rawdata, sub_boro_names)
-
-boros <- rawdata %>% filter(c(boro_names)) %>% tibble()
-
-boro_tables <- function(rawdata, boro){
-  rawdata %>% filter(boro_names == boro)
-}
-
-count(rawdata, boro_names)
 
 onlyboros <- rawdata %>% 
   select(-sub_boro_names)
@@ -20,17 +12,16 @@ collapsed <-
   summarize_all(mean);
 collapsed
 
-
-c(col_names(rawdata) subway + disabled_population + public_housing
-
 aggregate(poverty_rate ~ boro_names, data = rawdata, mean)
 
 just_brooklyn <- filter(rawdata, boro_names=="Brooklyn");
-table(just_brooklyn$unemployment);
 
 just_brooklyn %>%
   group_by(unemployment) %>%
   tally() %>%
   arrange(n);
 
-v_unemployed <- filter(just_brooklyn, unemployment>0.1)  
+v_unemployed <- filter(just_brooklyn, unemployment>0.1)
+
+# ensure_directory("clean_data")
+write_csv(collapsed, "clean_data.csv")
