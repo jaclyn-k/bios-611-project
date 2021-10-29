4c688e588241540d8262abe1e224b5ba176b6a8a
Jaclyn Karasik <jkarasik@unc.edu>
2021-10-29 03:09:50 -0400

    Added figure target.

--- R_analysis.R
+++ R_analysis.R
@@ -1,2 +1,8 @@
+# Exploratory analysis --------------------------------------------------
+source("~/work/clean_data.R")
+source("~/work/utils.R")
 
-rawdata <- read.csv("~/work/NYC_Housing_Data.csv")
+my_first_plot <- ggplot(rawdata, aes(racial_diversity_index, median_rent)) + geom_point()
+
+
+ggsave("~/work/my_first_plot.png", my_first_plot)
