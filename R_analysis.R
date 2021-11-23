8ae51546bf38790bafbf49fd01d560b1c26c5b18
Jaclyn Karasik <jkarasik@unc.edu>
2021-11-23 14:02:38 -0500

    Broke up analysis document into 3 aims.

--- R_analysis.R
+++
@@ -1,7 +0,0 @@
-source("utils.R")
-
-clean_data <- read_csv("clean_data/clean_data.csv");
-
-my_first_plot <- ggplot(clean_data, aes(racial_diversity_index, median_rent)) + geom_point()
-
-ggsave("figures/my_first_plot.png", my_first_plot)
